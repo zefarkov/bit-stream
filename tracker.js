6738251ede52cb5dc86ee6366e6372ecfbe32b5c
Farrukh <65373267+zefarkov@users.noreply.github.com>
2026-04-22 20:10:19 +0500

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -24,27 +24,4 @@ function track() {
 
 track();
 window.addEventListener("hashchange", track);
-</script><script>
-function track() {
-  let eventType = "visit";
-
-  if (location.pathname.includes("/links") && location.hash) {
-    eventType = "card";
-  }
-
-  fetch("https://track.bit-stream.uz/", {
-    method: "POST",
-    headers: {"Content-Type":"application/json"},
-    body: JSON.stringify({
-      eventType,
-      page: location.href,
-      ref: document.referrer || "",
-      ua: navigator.userAgent
-    }),
-    keepalive: true
-  }).catch(() => {});
-}
-
-track();
-window.addEventListener("hashchange", track);
 </script>
